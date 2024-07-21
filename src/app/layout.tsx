f4064aa5477b52d93b7e78210c4b48042c2f8788
JaredvGraham <jgram153@icloud.com>
2024-07-21 18:01:40 -0400

    google ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,20 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7FS20SMXY8"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7FS20SMXY8');
+            `,
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
