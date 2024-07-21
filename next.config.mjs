61f03ff6962ba5bb850378863d5de0fa7442215c
JaredvGraham <jgram153@icloud.com>
2024-07-21 12:36:58 -0400

    icreace size limit in nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "20mb", // Set desired value here
+    },
+  },
+};
 
 export default withNextVideo(nextConfig);
