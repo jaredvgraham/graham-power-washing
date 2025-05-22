9dfe03a882fb4c6d18fddc92a9fa9188272b2997
Jared Graham <jgram153@icloud.com>
2025-05-22 16:59:49 -0400

    checking drive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
-
+//
 export const metadata: Metadata = {
   title:
     "Graham Power Washing | Top-Rated Power Washing Services in Plymouth, MA | Free Quotes",
